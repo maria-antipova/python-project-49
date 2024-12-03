d015b0476ed20007eb5bc957d5cc21e090953087
Maria Antipova <maria_antipova562@mail.ru>
2024-12-03 20:20:53 +0300

    part3

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+from prompt_toolkit import prompt
+
+def welcome_user():
+    """Welcoming message, ask name and print greeting"""
+    print("Welcome to the Brain Games!")
+    name = prompt("May I have your name? ")
+    print(f"Hello, {name}!")
